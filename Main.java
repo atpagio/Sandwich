3ccb3ed12a5adb69fd404b3a6c250c08bc043d78
Student: Jenna Jenna <94848871+atpagio@users.noreply.github.com>
2021-12-02 23:37:02 +0000

    added cut the sandwich method

--- Main.java
+++ Main.java
@@ -1,5 +1,75 @@
+import java.io.*;
+import java.util.*;
+import static java.lang.System.*;
+
 class Main {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    Sandwich sw=new Sandwich();
+    Sandwich sw1=new Sandwich();
+    System.out.println(sw);
+    System.out.println("Would you like a custom sandwich? Or would you like to try a chef's choice?");
+    String option=scan.nextLine();
+    option=option.toLowerCase();
+    if(option.equals("custom"))
+    {
+    //customizer options
+    System.out.println("Sandwich customizer:\nWhat meat?");
+    String a=scan.nextLine();
+    System.out.println("Cheese?");
+    String b=scan.nextLine();
+    System.out.println("Vegetables?");
+    String c=scan.nextLine();
+    System.out.println("Bread type?");
+    String d=scan.nextLine();
+    System.out.println("Sauce/dressing?");
+    String e=scan.nextLine();
+    System.out.println("How long would you like it(inches)?");
+    int f=scan.nextInt();
+    System.out.println("Toasted?");
+    Scanner scan1 = new Scanner(System.in);
+    String g=scan1.nextLine();
+    g=g.toLowerCase();
+    System.out.println("Would you like to cut the sandwich up?");
+    String cut=scan.nextLine();
+    cut=cut.toLowerCase();
+    int cut1=0;
+    int num=1;
+    if(cut.equals("yes"))
+    {
+      System.out.println("What number of pieces would you like it cut into?");
+       num=scan1.nextInt();
+       cut1++;
+    }
     
+    boolean h=true;//
+    if(g.equals("yes"))
+    {
+      h=true;
+    } 
+    if(g.equals("no"))
+    {
+      h=false;
+    }
+    sw1=new Sandwich(a,b,c,d,e,f,h);
+    if(cut1>0)
+    {
+      sw1.cutSw(num);
+    }
+    System.out.println(sw1);
+    } else if(option.equals("chef's choice")||option.equals("chefs choice"))
+    {
+      sw1=new Sandwich().chefsChoice();
+      System.out.println(sw1);
+    } else {
+      sw1=new Sandwich();
+    }
+    System.out.println("\nDid you change the sandwich up? ");
+    if(!sw1.equals(sw))
+    {
+      System.out.print("It's a different sandwich!");
+    } else if(sw1.equals(sw)) {
+      System.out.print("It's the same type...");
+    }
   }
 }
--- Sandwich.java
+++ Sandwich.java
@@ -1,42 +1,161 @@
 public class Sandwich
 {
-  //declaring meat, cheese, and bread, cheese is set at certain amount
   private String meat;
   private String cheese;
-  private String bread;
   private String veggies;
-  private int numMeats;
-  private int numVeggies;
-  private boolean pickMeat;
-  private boolean pickCheese;
-  private boolean pickVeggies;
+  private String bread;
+  private String sauce;
+  private int length;
+  private boolean toasted;
+  int numCount=0;
 
   //default
   public Sandwich()
   {
-    //this("ham","swiss","lettuce","Italian")
+    this("liver","blue cheese","brussel sprouts","cinnamon bread","hot sauce",1,true);
   }
-  //parameters of meat, cheese, bread, and veggies
-  public Sandwich(String a, String b, String c, String d)
+
+  //user-entered parameters
+  public Sandwich(String a,String b,String c,String d,String e,int f,boolean g)
   {
-    meat=a;
-    cheese=b;
-    veggies=c;
-    bread=d;
+    setMeat(a);
+    setCheese(b);
+    setVeggies(c);
+    setBread(d);
+    setSauce(e);
+    setLength(f);
+    setToasted(g);
   }
 
   //accessors
+  public String getMeat()
+  {
+    return meat;
+  }
+  public String getCheese()
+  {
+    return cheese;
+  }
+  public String getVeggies()
+  {
+    return veggies;
+  }
+  public String getBread()
+  {
+    return bread;
+  }
+  public String getSauce()
+  {
+    return sauce;
+  }
+  public int getLength()
+  {
+    return length;
+  }
+  public boolean isItToasted()
+  {
+    return toasted;
+  }
 
   //mutators
+  public void setMeat(String a)
+  {
+    meat=a;
+  }
+  public void setCheese(String a)
+  {
+    cheese=a;
+  }
+  public void setVeggies(String a)
+  {
+    veggies=a;
+  }
+  public void setBread(String a)
+  {
+    bread=a;
+  }
+  public void setSauce(String a)
+  {
+    sauce=a;
+  }
+  public void setLength(int a)
+  {
+    length=a;
+  }
+  public void setToasted(boolean a)
+  {
+    toasted=a;
+  }
 
-  //pick a random meat
+  //pick a random choice!
+  public Sandwich chefsChoice()
+  {
+    int value = (1+((int)(3*Math.random())));
+    if(value==1)
+    {
+       Sandwich a=new Sandwich("ham","swiss","lettuce","Italian","caeser dressing",6,false);
+       a.cutSw(3);
+       return a;
+    }
+    if(value==2)
+    {
+       Sandwich a=new Sandwich("smoked chicken","queso fresco","hot pepper","brown bread","soy sauce",15,true);
+       a.cutSw(2);
+       return a;
+    }
+    if(value==3)
+    {
+       Sandwich a=new Sandwich("soppressata","havarti","spinach and mangosalsa","french roll","grainy mustard and tamari",15,true);
+       return a;
+    } 
+    Sandwich a= new Sandwich();
+    return a;
+  }
 
-  //equals method
+  //cut the sandwich?
+  public void cutSw(int b)
+  {
+    length/=b;
+    numCount+=b;
+  }
 
-  //toString method
+  //equals
+  public boolean equals(Sandwich a)
+{
+  a=new Sandwich();
+  if(this.getMeat()==a.getMeat()&&this.getCheese()==a.getCheese()&&this.getVeggies()==a.getVeggies()&&this.getBread()==a.getBread()&&this.getSauce()==a.getSauce())
+  {
+    return true;
+  } else {
+    return false;
+  }
+}
+  //toString, prints equals method then description of sandwich
   public String toString()
   {
+    Sandwich a=new Sandwich();
     String str="";
+    if(numCount>1)
+    {
+      str=(numCount+" "+meat+", "+cheese+", and "+veggies+" "+length+"in sandwiches on "+bread+", with "+sauce+", ");
+    } else {
+    str=("A "+meat+", "+cheese+", and "+veggies+" "+length+"in sandwich on "+bread+", with "+sauce+", ");
+    }
+    //if its toasted or not
+    if(toasted==true)
+    {
+      str+="toasted.";
+    } 
+    if(toasted==false){
+      str+="untoasted.";
+    }
+    //equals method for varying messages
+    if(this.equals(a))
+    {
+      System.out.println("This one sounds kind of nasty... how about making something better?");
+    } else {
+      System.out.println("\nThat's a fantastic choice!");
+    }
     return str;
   }
 }
